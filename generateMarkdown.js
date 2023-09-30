b958f597c3ed25af2ca2de8f5a84ba1c662d6635
Quackingtaco <r2kayla@gmail.com>
2023-09-29 21:35:28 -0400

    scripting code added

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,18 +1,118 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+        if (license.length === 0) {
+          return ""
+        } else if (license == "Apache") {
+          return `[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`
+        } else if (license == "GNU") {
+          return `[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)`
+        } else if (license == "MIT") {
+          return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`
+        } else if (license == "BSD 2-Clause") {
+          return `[![License](https://img.shields.io/badge/License-BSD_2--Clause-orange.svg)](https://opensource.org/licenses/BSD-2-Clause)`
+        } else if (license == "BSD 3-Clause") {
+          return `[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)`
+        } else if (license == "Boost") {
+          return `[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)`
+        } else if (license === 'CreativeCommons 0') {
+          return `[![License: CC0-1.0](https://licensebuttons.net/l/zero/1.0/80x15.png)](http://creativecommons.org/publicdomain/zero/1.0/)`
+        } else if (license === 'Eclipse') {
+          return `[![License](https://img.shields.io/badge/License-EPL_1.0-red.svg)](https://opensource.org/licenses/EPL-1.0)`
+        } else if (license === 'GNU AGPL') {
+          return `[![License: AGPL v3](https://img.shields.io/badge/License-AGPL_v3-blue.svg)](https://www.gnu.org/licenses/agpl-3.0)` 
+        } else if (license === 'GNU GPL v2') {
+          return `[![License: GPL v2](https://img.shields.io/badge/License-GPL_v2-blue.svg)](https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html)`
+        } else if (license === 'GNU LGPL') {
+          return `[![License: LGPL v3](https://img.shields.io/badge/License-LGPL_v3-blue.svg)](https://www.gnu.org/licenses/lgpl-3.0)`
+        } else if (license === 'Mozilla') {
+          return `[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)`
+        } else if (license === 'Unlicense') {
+          return `[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)`
+        }
+      };
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+        if (license.length === 0) {
+        return ""
+        } else if (license == "Apache") {
+        return `(https://opensource.org/licenses/Apache-2.0)`
+        } else if (license == "GNU") {
+        return `(https://www.gnu.org/licenses/gpl-3.0)`
+        } else if (license == "MIT") {
+        return `(https://opensource.org/licenses/MIT)`
+        } else if (license == "BSD 2-Clause") {
+        return `(https://opensource.org/licenses/BSD-2-Clause)`
+        } else if (license == "BSD 3-Clause") {
+        return `(https://opensource.org/licenses/BSD-3-Clause)`
+        } else if (license == "Boost") {
+        return `(https://www.boost.org/LICENSE_1_0.txt)`
+        } else if (license === 'CreativeCommons 0') {
+        return `(http://creativecommons.org/publicdomain/zero/1.0/)`
+        } else if (license === 'Eclipse') {
+        return `(https://opensource.org/licenses/EPL-1.0)`
+        }  else if (license === 'GNU AGPL') {
+        return `(https://www.gnu.org/licenses/agpl-3.0)` 
+        } else if (license === 'GNU GPL v2') {
+        return `(https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html)`
+        } else if (license === 'GNU LGPL') {
+        return `(https://www.gnu.org/licenses/lgpl-3.0)`
+        } else if (license === 'Mozilla') {
+        return `(https://opensource.org/licenses/MPL-2.0)`
+        } else if (license === 'Unlicense') {
+        return `(http://unlicense.org/)`
+        }
+    };
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+    if (license.length === 0) {
+        return ""
+      } else {
+        return `## License
+      ${renderLicenseBadge(license)}
+      ${renderLicenseLink(license)} 
+        `
+      }
+
+};
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  ${renderLicenseBadge(data.license)}
+
+  ## Description
+  ${data.description}
+  
+  ## Table of Contents
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [License](#license)
+  * [Contributing](#contributing)
+  * [Tests](#tests)
+  * [Questions](#questions)
+
+  ## Installation
+  ${data.install}
+
+  ## Usage
+  ${data.usage}
+
+  ## Tests
+  ${data.test}
+
+  ## Contributing
+  ${data.contribute}
+
+  ${renderLicenseSection(data.license)}
+
+  ## Questions
+  GitHub: [${data.username}](https://github.com/${data.username}) <br>
+  Email: ${data.email}
 
 `;
 }
--- index.js
+++ index.js
@@ -1,13 +1,119 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const generateMarkdown = require("generateMarkdown");
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        message: "What is your GitHub Username?",
+        name: 'username',
+        validate: function(answer) {
+            if (answer.length < 1) {
+                return console.log("Please provide a valid GitHub username.")
+            }
+            return true;
+        }
+    },
+    {
+        type: 'input',
+        message: "What is your e-mail address?",
+        name: 'email',
+        validate: function(answer) {
+            if (answer.length < 1) {
+                return console.log("Please provide a valid e-mail address.")
+            }
+            return true;
+        }
+    },
+    {
+        type: 'input',
+        message: "What is the name of your GitHub repository?",
+        name: 'repo',
+        validate: function(answer) {
+            if (answer.length < 1) {
+                return console.log("Please provide a valid repository name.")
+            }
+            return true;
+        }
+    },
+    {
+        type: 'input',
+        message: "What is the title of your project?",
+        name: 'title',
+        validate: function(answer) {
+            if (answer.length < 1) {
+                return console.log("Please provide a valid project title.")
+            }
+            return true;
+        }
+    },
+    {
+        type: 'input',
+        message: "Write a description of your project.",
+        name: 'description',
+        validate: function(answer) {
+            if (answer.length < 1) {
+                return console.log("Please provide a valid project description.")
+            }
+            return true;
+        }
+    },
+    {
+        type: 'input',
+        message: "Please provide installation instructions.",
+        name: 'install',
+    },
+    {
+        type: 'input',
+        message: "Please provide project usages.",
+        name: 'usage',
+    },
+    {
+        type: 'input',
+        message: "Please provide contribution guidelines for other users.",
+        name: 'contribute',
+    },
+    {
+        type: 'input',
+        message: "Provide and tests written or used for your project.",
+        name: 'test',
+    },
+    {
+        type: 'list',
+        message: "Choose a license for your project.",
+        choices: ['Apache', 'Mozilla', 'MIT', 'GNU', 'Boost', 'ISC'],
+        name: 'license',
+    },
+
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, generateMarkdown(data), err => {
+        if (err) {
+            return console.log(err);
+        }
+        console.log("README creation was a success!")
+    })
+
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.createPromptModule(questions)
+    .then(function(answers) {
+        const fileName = 
+        answers.title
+        .split('')
+        .join('') + '.md';
+        writeToFile(fileName, answers);
+    }
+    
+    );
+
+}
 
 // Function call to initialize app
 init();
